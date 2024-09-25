477d714db0b64630b56bd6958ba1dc4fa3828e75
Yehor Markhaichuk <yehormarkhaichuk@gmail.com>
2024-09-25 18:04:53 +0300

    feat: basic functionality

---
+++ app/config.py
@@ -0,0 +1,6 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "<KEY>"
+app.config["SQLALCHEMY_DATABASE_URI"] ="sqlite:///db.sqlite3"
---
+++ app/views.py
@@ -0,0 +1,9 @@
+from .config import app
+
+from flask import render_template
+
+
+@app.route("/index")
+@app.route("/")
+def index():
+    return render_template("index.html")
