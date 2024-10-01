3fcabc2e742b81761a60b4e6d31a8f6afd584e3a
Yehor Markhaichuk <yehormarkhaichuk@gmail.com>
2024-10-01 18:52:10 +0300

    feat: Implemented registration

--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
-from .config import app
+from flask import render_template, request, redirect, make_response
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.exc import IntegrityError
 
-from flask import render_template
+from .config import *
+from .db import *
+
+
+__all__ = ("index", "rent")
 
 
 @app.route("/index")
@@ -12,3 +18,21 @@ def index():
 @app.route("/rent")
 def rent():
     return render_template("rent.html")
+
+
+@app.route("/register", methods=["POST"])  # Responsible for processing user registration.
+def register():
+    print("success")
+    username = request.form["username"]
+    email = request.form["email"]
+    password = request.form["password"]
+    password_hash = generate_password_hash(password)
+    print(username, email, password_hash)
+    user = User(username=username, email=email, password_hash=password_hash)
+    try:
+        db.session.add(user)
+        db.session.commit()
+        return "Success"
+    except IntegrityError:
+        db.session.rollback()
+        return "This user already exists try again."
